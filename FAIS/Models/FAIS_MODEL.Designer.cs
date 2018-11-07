b35ab48884d2ff205bfcd0aab38df6ab56c3cfc9
Mohamed Rmili <simorml@gmail.com>
2018-11-07 12:40:52 +0100

    a

--- FAIS/Models/FAIS_MODEL.Designer.cs
+++ FAIS/Models/FAIS_MODEL.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'D:\PERSO\FAIS_GIT\admin.mvc.js\FAIS\Models\FAIS_MODEL.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\www\_.net\NB\admin.mvc.js\FAIS\Models\FAIS_MODEL.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
