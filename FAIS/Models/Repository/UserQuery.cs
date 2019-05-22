1eef148a504f39cf6f5f962e43afb110a279dcce
Mohamed Rmili <simorml@gmail.com>
2019-05-22 16:31:22 +0000

    Widget: Query

---
+++ FAIS/Models/Repository/UserQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace FAIS.Models.Repository
+{
+    public class UserQuery
+    {
+        public string sqlQuery { get; set; }
+
+        public bool validate()
+        {
+            if (this.sqlQuery.ToLower().Contains("--")) return false;
+            if (this.sqlQuery.ToLower().Contains("drop ")) return false;
+            if (this.sqlQuery.ToLower().Contains("update ")) return false;
+            if (this.sqlQuery.ToLower().Contains("exec ")) return false;
+            if (this.sqlQuery.ToLower().Contains("revoke ")) return false;
+            if (this.sqlQuery.ToLower().Contains("grant ")) return false;
+            if (this.sqlQuery.ToLower().Contains("create ")) return false;
+            if (this.sqlQuery.ToLower().Contains("alter ")) return false;
+            if (this.sqlQuery.ToLower().Contains("into ")) return false;
+            if (this.sqlQuery.ToLower().Contains("insert ")) return false;
+            if (this.sqlQuery.ToLower().Contains("sp_")) return false;
+            if (this.sqlQuery.ToLower().Contains("delete ")) return false;
+
+            return true;
+        }
+
+        public DataTable Execute()
+        {
+            return new SGBD().Cmd(this.sqlQuery);
+        }
+    }
+}
